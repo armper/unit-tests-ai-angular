7dd296122bd4fa9151747a00858ab523237338be
Armando Perea <aleoperea@yahoo.com>
2024-02-21 13:18:42 -0500

    test

--- src/app/patient-list/patient-list.component.spec.ts
+++ src/app/patient-list/patient-list.component.spec.ts
@@ -2,16 +2,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { Patient } from '../patient/patient';
 import { PatientListComponent } from './patient-list.component';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-@Component({
-  selector: 'app-patient',
-  template: ''
-})
-class MockPatientComponent {
-  @Input() patient: Patient;
-  @Output() patientUpdated = new EventEmitter<Patient>();
-}
 
 describe('PatientListComponent', () => {
   let component: PatientListComponent;
@@ -19,7 +9,7 @@ describe('PatientListComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PatientListComponent, MockPatientComponent]
+      declarations: [PatientListComponent]
     });
     fixture = TestBed.createComponent(PatientListComponent);
     component = fixture.componentInstance;
@@ -30,20 +20,5 @@ describe('PatientListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should update patient when onPatientUpdated is called', () => {
-    const patient: Patient = { name: 'John', details: 'details' };
-    const updatedPatient: Patient = { name: 'John', details: 'updated details' };
-    component.patients = [patient];
-    component.onPatientUpdated(updatedPatient);
-    expect(component.patients[0]).toEqual(updatedPatient);
-  });
-
-  it('should update patient when patientUpdated event is emitted', () => {
-    const patient: Patient = { name: 'John', details: 'details' };
-    const updatedPatient: Patient = { name: 'John', details: 'updated details' };
-    component.patients = [patient];
-    const patientComponent = fixture.debugElement.query(By.directive(MockPatientComponent));
-    patientComponent.triggerEventHandler('patientUpdated', updatedPatient);
-    expect(component.patients[0]).toEqual(updatedPatient);
-  });
+ 
 });
